3430bec7e94d64442263aeda68091cfa6d50edfc
Aaron Keohane <aakeohane@gmail.com>
2023-08-01 16:39:54 -0700

    update message board statement when rooms may be available

--- firebase.js
+++ firebase.js
@@ -19,12 +19,36 @@ const lists = document.getElementsByClassName("roomList")
 
 let timeout
 
+function clearBoard() {
+  while(messageBoard.firstElementChild) {
+    messageBoard.firstElementChild.remove();
+ }
+  clearInterval(timeout)
+}
+
+onValue(child(dbRef, '/'), (snapshot) => {
+  const totalValue = snapshot.val()
+  // object destructuring
+  const {Announcements: {"Locker Number Ready": LockerNumber}, "Waiting Lists": WaitingLists} = totalValue
+  const isLockerReadyEmpty = (LockerNumber == "")
+  const waitlistArr = Object.values(WaitingLists)
+  const iswaitlistEmpty = waitlistArr.some(item => item == "")
+  // if both are true (they are blank strings) then there are rooms available so waitlist is not needed
+  let availabilityArray
+  if (isLockerReadyEmpty && iswaitlistEmpty) {
+    availabilityArray = ["yes"]
+    clearBoard()
+    createAnncmt(availabilityArray)
+  } else if (isLockerReadyEmpty && !iswaitlistEmpty) {
+    availabilityArray = LockerNumber
+    clearBoard()
+    createAnncmt(availabilityArray)
+  }
+})
+
 onValue(child(dbRef, `Announcements/Locker Number Ready`), (snapshot) => {
   if (snapshot.exists()) {
-    while(messageBoard.firstElementChild) {
-      messageBoard.firstElementChild.remove();
-   }
-    clearInterval(timeout)
+    clearBoard()
     const firebaseArray = snapshot.val()
     createAnncmt(firebaseArray)
   }
@@ -37,7 +61,7 @@ onValue(child(dbRef, `Waiting Lists/`), (snapshot) => {
       const nodes = document.getElementsByClassName("lockerContainer")
       const nodeList = [...nodes]
       const filteredArray = []
-      // filters out the animated secondary node because it causes parent node glitch
+      // filters out the animated cloned secondary node because it causes parent node glitch
       nodeList.map(item => {
         if (!item.classList.contains("animate-list-secondary")) {
           filteredArray.push(item)
@@ -75,8 +99,6 @@ function addLockers(listDiv, groupTitle) {
         }
         ))
       } animateList(listDiv)
-    } else {
-      console.log("No data available");
     }
   }).catch((error) => {
     console.error(error);
@@ -100,15 +122,21 @@ function createAnncmt(arr) {
   const messageOptionsArr = ["cum to", "sashay toward", "bounce that booty to"]
   const randomMessageChoice = messageOptionsArr[Math.floor(Math.random() * messageOptionsArr.length)]
   let delay
-  if (arr == "") {
-    const template = "There are currently no rooms available. Thanks for your patience!"
+  let template
+  if (arr == "yes") {
+    template = "There may be some rooms available. Please ask the front desk for assistance."
+    messageArr.push(template)
+    delay = 10000
+  }
+  else if (arr == "") {
+    template = "There are currently no rooms available. Thanks for your patience!"
     messageArr.push(template)
     delay = 10000
   } else {
     arr.map(item => {
       let lockerNum = item["Locker Number"]
       let room = item["Room"]
-      const template = `${lockerNum}, your ${room} ${(room == "Non TV" || room == "Regular TV" || room == "Large TV") ? "room" : ""} is ready! Please ${randomMessageChoice} the front desk.`
+      template = `${lockerNum}, your ${room} ${(room == "Non TV" || room == "Regular TV" || room == "Large TV") ? "room" : ""} is ready! Please ${randomMessageChoice} the front desk.`
       // unshift allows me to announce the locker number ready immediately
       messageArr.unshift(template)
     })
@@ -150,5 +178,3 @@ function animateList(roomType) {
     clone.classList.add("animate-list-secondary")
   }
 }
-
-// message on bottom saying thanks for waiting etc.
